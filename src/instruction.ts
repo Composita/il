a704a48e2038182acfbbd97a58a4eb7bb48a118c
Hansruedi Patzen <hp@revy.ch>
2020-07-21 17:19:10 +0200

    instructions: wrap operands in types

--- src/instruction.ts
+++ src/instruction.ts
@@ -46,7 +46,68 @@ export enum SystemCall {
     WriteLine,
 }
 
-export type InstructionOperand = number | boolean | string | SystemCall | Descriptor;
+enum TextValueTag {
+    Tag,
+}
+export class TextValue {
+    constructor(public readonly value: string) {}
+
+    protected readonly _textValueTag = TextValueTag.Tag;
+}
+
+enum CharacterValueTag {
+    Tag,
+}
+export class CharacterValue {
+    constructor(public readonly value: string) {}
+
+    protected readonly _characterValueTag = CharacterValueTag.Tag;
+}
+
+enum FloatValueTag {
+    Tag,
+}
+export class FloatValue {
+    constructor(public readonly value: number) {}
+
+    protected readonly _floatValueTag = FloatValueTag.Tag;
+}
+
+enum IntegerValueTag {
+    Tag,
+}
+export class IntegerValue {
+    constructor(public readonly value: number) {}
+
+    protected readonly _integerValueTag = IntegerValueTag.Tag;
+}
+
+enum BooleanValueTag {
+    Tag,
+}
+export class BooleanValue {
+    constructor(public readonly value: boolean) {}
+
+    protected readonly _booleanValueTag = BooleanValueTag.Tag;
+}
+
+enum SystemCallValueTag {
+    Tag,
+}
+export class SystemCallValue {
+    constructor(public readonly value: SystemCall) {}
+
+    protected readonly _systemCallValueTag = SystemCallValueTag.Tag;
+}
+
+export type InstructionOperand =
+    | IntegerValue
+    | FloatValue
+    | BooleanValue
+    | CharacterValue
+    | TextValue
+    | SystemCallValue
+    | Descriptor;
 
 export class Instruction {
     constructor(private readonly opCode: OpCode, ...operands: Array<InstructionOperand>) {
