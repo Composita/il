0c57d30a2c2753f7d9427f84cb032396fd2d72d4
Hansruedi Patzen <hp@revy.ch>
2020-11-26 00:09:29 +0100

    instruction: add args ctor

--- src/instruction.ts
+++ src/instruction.ts
@@ -8,9 +8,9 @@ import {
 import { OperatorCode } from './opcode';
 
 export class Instruction {
-    constructor(code: OperatorCode) {
+    constructor(code: OperatorCode, ...args: Array<InstructionArgument>) {
         this.code = code;
-        this.arguments = new Array<InstructionArgument>();
+        this.arguments = new Array<InstructionArgument>(...args);
     }
     public readonly code: OperatorCode;
     public readonly arguments: Array<InstructionArgument>;
